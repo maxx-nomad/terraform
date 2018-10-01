debe778c0c53287420e9f12e795539b148332634
Maksim <maksim.kolesnikov@weblabs.pro>
2018-10-01 03:16:31 +0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# terraform
+Run with a command like this:
+
+terraform apply -var 'key_name={your_aws_key_name}' \
+   -var 'public_key_path={location_of_your_key_in_your_local_machine}'
+
+For RDS user
+Pass the password variable through your ENV variable.
---
+++ main.tf
@@ -0,0 +1,104 @@
+provider "aws" {
+  region                  = "eu-central-1"
+  shared_credentials_file = "/Users/maxx/.aws/credentials"
+  profile                 = "default"
+}
+
+# Create a VPC
+resource "aws_vpc" "default" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Create an internet gateway
+resource "aws_internet_gateway" "default" {
+  vpc_id = "${aws_vpc.default.id}"
+}
+
+# Grant the VPC internet access on its main route table
+resource "aws_route" "internet_access" {
+  route_table_id         = "${aws_vpc.default.main_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.default.id}"
+}
+
+# Create a subnet
+resource "aws_subnet" "default" {
+  vpc_id                  = "${aws_vpc.default.id}"
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+# Security group to access the instances over SSH and HTTP
+resource "aws_security_group" "default" {
+  name        = "webapp"
+  description = "Used in the terraform"
+  vpc_id      = "${aws_vpc.default.id}"
+
+  # inbound SSH access
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # inbound HTTP access
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "auth" {
+  key_name   = "${var.key_name}"
+  public_key = "${file(var.public_key_path)}"
+}
+
+# Create ec2 instance
+resource "aws_instance" "webapp" {
+  # use ubuntu 18.04 server ami
+  ami = "ami-0bdf93799014acdc4"
+  instance_type = "t2.micro"
+
+  # The name of our SSH keypair we created above.
+  key_name = "${aws_key_pair.auth.id}"
+
+  vpc_security_group_ids = ["${aws_security_group.webapp.id}"]
+  subnet_id = "${aws_subnet.default.id}"
+
+  # Configure the instance with ansible-playbook
+  provisioner "local-exec" {
+    command = "ansible-playbook -u ubuntu --private-key ${var.public_key_path} -i '${aws_instance.webapp.public_ip},' provision.yml"
+  }
+}
+
+# Create rds instance
+resource "aws_db_instance" "default" {
+  identifier = "webapp-rds"
+  allocated_storage = "10"
+  engine = "postgres"
+  engine_version = "10.1-R1"
+  instance_class = "db.t2.micro"
+  name = "webappdb"
+  username = "webapp_user"
+  password = "${var.password}"
+}
+
+# RDS Security Group
+resource "aws_db_security_group" "default" {
+  name = "webapp_rds_sg"
+
+  ingress {
+    cidr = "10.0.1.0/24"
+  }
+}
